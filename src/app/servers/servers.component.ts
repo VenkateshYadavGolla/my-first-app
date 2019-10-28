c367229f1ca90b362455f97ac362820eb8d53962
VenkateshYadavGolla <g.venkatesh.yadav1992@gmail.com>
2019-10-28 00:29:51 -0400

    Angular Data Binding and predifined Directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,8 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server Created as of now';
+  serverName = 'TestServer';
 
-  constructor() { }
+  servers = ['TestServer', 'TestServer2'];
+
+  serverCreated = false;
+
+  constructor() {
+    setTimeout( () => { this.allowNewServer = true; }, 2000);
+  }
+
+  onCreateServer () {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server Created! Name is ' + this.serverName ;
+  }
+
+  onUpdateServerName (event : Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+    console.log(event);
+  }
 
   ngOnInit() {
   }
